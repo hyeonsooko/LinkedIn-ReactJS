611b572695a8613bab6c2ce6c477b905243826c6
Soo Ko <hko@uab.edu>
2024-06-22 21:22:58 -0500

    google button

--- LinkedIn ReactJS/src/App.jsx
+++ LinkedIn ReactJS/src/App.jsx
@@ -1,12 +1,25 @@
 import Login from './components/Login.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ErrorPage from "./error-page";
+import Home from "./components/Home.jsx"
+import Header from './components/Header.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (<Login />),
+    element: (
+      <Login />
+    ),
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "home",
+    element: (
+      <>
+        <Header />
+        <Home />
+      </>
+    ),
   },
 ]);
 
